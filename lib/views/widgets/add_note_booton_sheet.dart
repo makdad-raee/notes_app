d4d168985a95f59eb08160a6475e32cf12e64933
makdad-raee <makdadraeeeeee@gmail.com>
2023-10-17 15:12:37 +0300

    Add field to button sheet

--- lib/views/widgets/add_note_booton_sheet.dart
+++ lib/views/widgets/add_note_booton_sheet.dart
@@ -13,7 +13,11 @@ class AddNoteBottonSheet extends StatelessWidget {
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(hint: 'Title'),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(hint: 'Content', maxLines: 5),
         ],
       ),
     );
