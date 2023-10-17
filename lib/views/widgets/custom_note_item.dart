7f873b76144d1cfff74c99f9baa9054854cb19dc
makdad-raee <makdadraeeeeee@gmail.com>
2023-10-17 17:30:42 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Text(
-                'Flutter tips',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const EditNoteView(),
+        ));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child: Text(
+                  'Flutter tips',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 26,
+                  ),
                 ),
               ),
+              subtitle: Text('Build your carrer with tharwat sami ',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 18)),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    size: 30,
+                    color: Colors.black,
+                  )),
             ),
-            subtitle: Text('Build your carrer with tharwat sami ',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 18)),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  size: 30,
-                  color: Colors.black,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('December21 , 2023',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 16)),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text('December21 , 2023',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 16)),
+            )
+          ],
+        ),
       ),
     );
   }
