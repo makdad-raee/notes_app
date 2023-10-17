ffea41813cd1382827ed9a69071040a7f137d053
makdad-raee <makdadraeeeeee@gmail.com>
2023-10-17 17:14:42 +0300

    finish notes button sheet

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.w800),
+        ),
+      ),
+    );
+  }
+}
