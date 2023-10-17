474a1a70b6623bf64288a217101603b86ef3cbf1
makdad-raee <makdadraeeeeee@gmail.com>
2023-10-17 17:55:07 +0300

    edit note view UI done

--- lib/views/widgets/edit_not_view_body.dart
+++ lib/views/widgets/edit_not_view_body.dart
@@ -15,7 +15,13 @@ class EditNoteViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppar(title: 'Edit Note', icon: Icons.check),
+          SizedBox(
+            height: 50,
+          ),
           CustomTextField(hint: 'Title'),
+          SizedBox(
+            height: 16,
+          ),
           CustomTextField(hint: 'Content', maxLines: 5),
         ],
       ),
