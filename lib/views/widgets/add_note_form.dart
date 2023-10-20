f8181cf8091f44331c3b5e7d9a33c3f4ac8f48d7
makdad-raee <makdadraeeeeee@gmail.com>
2023-10-20 18:08:56 +0300

    format date time

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -5,6 +5,7 @@ import 'package:notes_app/cubits/states.dart';
 import 'package:notes_app/models/notesmodel.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
+import 'package:intl/intl.dart';
 
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
@@ -55,10 +56,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
+                    DateTime currentDate = DateTime.now();
+                    String formattedDate = DateFormat.yMd().format(currentDate);
+
                     var notModel = NotesModel(
                         title: title!,
                         subTitle: subTitle!,
-                        date: DateTime.now().toString(),
+                        date: formattedDate,
                         color: Colors.black.value);
                     BlocProvider.of<NotesCubit>(context).addNote(notModel);
                   } else {
